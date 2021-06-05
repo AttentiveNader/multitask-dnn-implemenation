feb591076fc3425ba87a405c7818a32316e8ff11
NaderJS <nader_atef80@outlook.com>
2021-06-05 02:12:02 +0200

    added encoder layer beta

--- model/contextualEncoder.py
+++ model/contextualEncoder.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-
+from .myEncoder import  EncoderLayer
 # todo
 """
 write the transformerEncoder implementation  and replace the pytorch one 
@@ -12,7 +12,8 @@ class TransformerEncoder(nn.Module):
 
     def __int__(self,d_model:int,n_heads:int,n_layers:int,implementation:str):
         assert (d_model % n_heads == 0) , "the embedding dimension should be divisible by the number of heads"
-        encoderLayer = nn.TransformerEncoderLayer(d_model,n_heads)
+        # encoderLayer = nn.TransformerEncoderLayer(d_model,n_heads)
+        encoderLayer = EncoderLayer(d_model,n_heads)
         self.contextualEncoder = nn.TransformerEncoder(encoderLayer,n_layers)
 
     def forward(self,x,padding_mask)->torch.Tensor:
--- model/myEncoder.py
+++ model/myEncoder.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch
+from torch.nn import functional as F
 
 
 """
@@ -13,9 +14,7 @@ this implementation depends highly on torch.einsum
 figure out  whether the projection weight matrices for q,v and k are different for every head 
 or it's just a big weight matrix before splitting into heads(am using the latter for now)
 
-also implement the feedforward network
 """
-
 
 
 class MultiHeadAttention(nn.Module):
@@ -27,6 +26,8 @@ class MultiHeadAttention(nn.Module):
         self.keys = nn.Linear(self.d_model,self.d_model,bias=False)
         self.values = nn.Linear(self.d_model,self.d_model,bias=False)
         self.fc_out = nn.Linear(d_model,d_model)
+        self.dropout = nn.Dropout(0.1)
+        self.layerNorm = nn.LayerNorm(d_model,eps=1e-6)
 
     def forward(self,queries,keys,values,mask)->torch.Tensor:
 
@@ -34,21 +35,40 @@ class MultiHeadAttention(nn.Module):
         keys = self.keys(keys)
         values = self.values(values)
 
-        queries = queries.reshape(queries.shape[0],queries.shape[1],self.n_heads,self.d_head)
-        keys = keys.reshape(keys.shape[0],keys.shape[1],self.n_heads,self.d_head)
-        values = values.reshape(values.shape[0],values.shape[1],self.n_heads,self.d_head)
+        q = queries.reshape(queries.shape[0],queries.shape[1],self.n_heads,self.d_head)
+        k = keys.reshape(keys.shape[0],keys.shape[1],self.n_heads,self.d_head)
+        v = values.reshape(values.shape[0],values.shape[1],self.n_heads,self.d_head)
 
-        attn = torch.einsum("nqhd,nkhd->nhqk",[queries,keys])
+        attn = torch.einsum("nqhd,nkhd->nhqk",q,k)
 
         if mask is not None:
-            attn = attn.masked_fill(mask == 0,float('1e-18'))
+            attn = attn.masked_fill(mask == 0,float('-inf'))
         attn = torch.softmax(attn/(self.d_model**0.5),dim=3)
 
         # out = torch.einsum('nhqk,nvhd->nvhd')
-        out = torch.einsum('nhql,nlhd->nqhd'[attn,values]).reshape(values.shape[0],values.shape[1],self.d_model)
+        out = torch.einsum('nhql,nlhd->nqhd',attn,v).reshape(values.shape[0],values.shape[1],self.d_model)
 
-        out = self.fc_out(out)
+        out = self.dropout(F.relu(self.fc_out(out)))
+        out += q
 
+        out = self.layerNorm(out)
+        return out
+
+
+class Intermediate(nn.Module):
+    def __int__(self,d_model:int,activation,intermediateSize:int=2048):
+
+        self.act1 = activation()
+        self.linear1 = nn.Linear(d_model,intermediateSize)
+        self.linear2 = nn.Linear(intermediateSize,d_model)
+        self.dropout = nn.Dropout(0.1)
+        self.layerNorm = nn.LayerNorm(d_model,eps=1e-6)
+
+    def forward(self,x):
+        res = x
+        out = self.dropout(self.act1(self.linear2(self.linear1(x))))
+        out += res
+        out = self.layerNorm(out)
         return out
 
 
@@ -57,3 +77,8 @@ class EncoderLayer(nn.Module):
     def __int__(self,d_model:int,n_heads):
         super(EncoderLayer, self).__int__()
         self.multiHeadAttention = MultiHeadAttention(d_model,n_heads)
+        self.feedForwardNetwork = Intermediate(d_model,nn.ReLU)
+
+    def forward(self,x,mask):
+        x = self.multiHeadAttention(x,mask)
+        return self.feedForwardNetwork(x)
